fe5633c6d814247f8a7d70bb795893e2eeb09fe5
salma akter <102917129+salmaakter11@users.noreply.github.com>
2025-04-23 17:56:19 +0600

    Update Project.jsx

--- src/page/Project.jsx
+++ src/page/Project.jsx
@@ -16,9 +16,9 @@ const Project = () => {
         { name: "Portfolio(Md-Monirul-Islam)", url: "https://www.themonirul.com/", imgSrc: "assets/img/Portfolio(Md-Monirul-Islam).JPG" },
         { name: "protyzer", url: "https://protyzermedicalsystem.com/", imgSrc: "assets/img/protyzer.png" },
         { name: "fastmarkcorporation", url: "https://www.fastmarkcorporation.com/", imgSrc: "assets/img/fast.png" },
-        { name: "jewel-design-ltd", url: "https://jeweldesign.com.bd/", imgSrc: "assets/img/jwel-design-ltd.png" },
-        { name: "SF Denim", url: "https://sfdenim.airticketbangladesh.com/", imgSrc: "assets/img/sf.png" }
-         { name: "Capital News", url: "https://www.capitalnews24.net/", imgSrc: "assets/img/NEWS.png" },
+        // { name: "jewel-design-ltd", url: "https://jeweldesign.com.bd/", imgSrc: "assets/img/jwel-design-ltd.png" },
+        // { name: "SF Denim", url: "https://sfdenim.airticketbangladesh.com/", imgSrc: "assets/img/sf.png" }
+        //  { name: "Capital News", url: "https://www.capitalnews24.net/", imgSrc: "assets/img/NEWS.png" },
 
       ]
     },
